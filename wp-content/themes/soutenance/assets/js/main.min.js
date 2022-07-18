deaf3f793d31b0b5ffc7245609b71cb6b6f34f4d
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-18 19:26:23 +0200

    feat(start-project): header + js

--- wp-content/themes/soutenance/assets/js/main.min.js
+++ wp-content/themes/soutenance/assets/js/main.min.js
@@ -1,7 +1,10 @@
-var buttons = [document.getElementById('closebtn'), document.getElementById('burgerMenu')];
-buttons.forEach(function (button) {
-  button.addEventListener('click', function () {
-    var mySidebar = document.getElementById('hidden-menu');
-    mySidebar.classList.toggle('--open');
+window.addEventListener("DOMContentLoaded", function (event) {
+  var buttons = [document.getElementById('closebtn'), document.getElementById('burgerMenu')];
+  console.log(buttons);
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var mySidebar = document.getElementById('hidden-menu');
+      mySidebar.classList.toggle('--open');
+    });
   });
 });
--- wp-content/themes/soutenance/src/js/main.js
+++ wp-content/themes/soutenance/src/js/main.js
@@ -1,10 +1,13 @@
-const buttons = [
-  document.getElementById('closebtn'),
-  document.getElementById('burgerMenu'),
-];
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const mySidebar = document.getElementById('hidden-menu');
-    mySidebar.classList.toggle('--open');
+window.addEventListener("DOMContentLoaded", (event) => {
+  const buttons = [
+    document.getElementById('closebtn'),
+    document.getElementById('burgerMenu'),
+  ];
+  console.log(buttons)
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const mySidebar = document.getElementById('hidden-menu');
+      mySidebar.classList.toggle('--open');
+    });
   });
 });
