ec79a643b1eeabfec2637c96af6d962469dcfdae
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-20 00:31:47 +0200

    feat(404): add 404 page

---
+++ wp-content/themes/soutenance/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<main id="404">
+  <h1>404 Error.</h1>
+  <p>The page you were looking for couldn't be found.<br>Maybe try a search?</p>
+  <?php get_search_form(); ?>
+</main>
+<?php get_footer(); ?>
