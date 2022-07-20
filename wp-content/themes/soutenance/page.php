fd0160b7d9f1e0f955bf5ae8dcb1fbca0f28ba8b
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-20 16:22:35 +0200

    feat(add-home): template parts : partners, services

--- wp-content/themes/soutenance/page.php
+++ wp-content/themes/soutenance/page.php
@@ -7,4 +7,6 @@
   <?php the_content() ?>
 </div>
 <?php include('template-parts/desc-block.php'); ?>
+<?php include('template-parts/our-services.php'); ?>
+<?php include('template-parts/our-partners.php'); ?>
 <?php get_footer() ?>
--- wp-content/themes/soutenance/template-parts/our-partners.php
+++ wp-content/themes/soutenance/template-parts/our-partners.php
@@ -1,3 +1,8 @@
 <div class="our-partners">
-
+  <img src="<?= get_theme_mod('partners_first') ?>" alt="">
+  <img src="<?= get_theme_mod('partners_second') ?>" alt="">
+  <img src="<?= get_theme_mod('partners_third') ?>" alt="">
+  <img src="<?= get_theme_mod('partners_fourth') ?>" alt="">
+  <img src="<?= get_theme_mod('partners_fifth') ?>" alt="">
+  <img src="<?= get_theme_mod('partners_sixth') ?>" alt="">
 </div>
