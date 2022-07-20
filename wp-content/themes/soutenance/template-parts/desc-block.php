7c2ef549f0b55f668c57dec0668bc727a9494972
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-21 01:18:56 +0200

    feat(style): wordpress

--- wp-content/themes/soutenance/template-parts/desc-block.php
+++ wp-content/themes/soutenance/template-parts/desc-block.php
@@ -1,11 +1,20 @@
 <div class="desc-block">
   <img src="<?= get_theme_mod('img_desc') ?>">
-  <h3><?= get_theme_mod('title_desc_first') ?></h3>
-  <p><?= get_theme_mod('paragraph_desc_first') ?></p>
 
-  <h3><?= get_theme_mod('title_desc_second') ?></h3>
-  <p><?= get_theme_mod('paragraph_desc_second') ?></p>
+  <div class="groups">
+    <div class="group">
+      <h3><?= get_theme_mod('title_desc_first') ?></h3>
+      <p><?= get_theme_mod('paragraph_desc_first') ?></p>
+    </div>
 
-  <h3><?= get_theme_mod('title_desc_third') ?></h3>
-  <p><?= get_theme_mod('paragraph_desc_third') ?></p>
+    <div class="group">
+      <h3><?= get_theme_mod('title_desc_second') ?></h3>
+      <p><?= get_theme_mod('paragraph_desc_second') ?></p>
+    </div>
+
+    <div class="group">
+      <h3><?= get_theme_mod('title_desc_third') ?></h3>
+      <p><?= get_theme_mod('paragraph_desc_third') ?></p>
+    </div>
+  </div>
 </div>
--- wp-content/themes/soutenance/template-parts/our-partners.php
+++ wp-content/themes/soutenance/template-parts/our-partners.php
@@ -1,8 +1,11 @@
 <div class="our-partners">
-  <img src="<?= get_theme_mod('partners_first') ?>" alt="">
-  <img src="<?= get_theme_mod('partners_second') ?>" alt="">
-  <img src="<?= get_theme_mod('partners_third') ?>" alt="">
-  <img src="<?= get_theme_mod('partners_fourth') ?>" alt="">
-  <img src="<?= get_theme_mod('partners_fifth') ?>" alt="">
-  <img src="<?= get_theme_mod('partners_sixth') ?>" alt="">
+  <h2>Our Partners</h2>
+  <div class="partners">
+    <img src="<?= get_theme_mod('partners_first') ?>" alt="">
+    <img src="<?= get_theme_mod('partners_second') ?>" alt="">
+    <img src="<?= get_theme_mod('partners_third') ?>" alt="">
+    <img src="<?= get_theme_mod('partners_fourth') ?>" alt="">
+    <img src="<?= get_theme_mod('partners_fifth') ?>" alt="">
+    <img src="<?= get_theme_mod('partners_sixth') ?>" alt="">
+  </div>
 </div>
