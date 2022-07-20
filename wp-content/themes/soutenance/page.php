9cb6612d54292fd654d400060f3b0dd6dd5e1e5e
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-20 15:31:00 +0200

    feat(add-home): block company infos

---
+++ wp-content/themes/soutenance/page.php
@@ -0,0 +1,10 @@
+<?php get_header() ?>
+<h1 class="page-title"><?php the_title() ?></h1>
+<div class="page-image">
+  <?= get_the_post_thumbnail() ?>
+</div>
+<div class="page-content">
+  <?php the_content() ?>
+</div>
+<?php include('template-parts/desc-block.php'); ?>
+<?php get_footer() ?>
---
+++ wp-content/themes/soutenance/template-parts/desc-block.php
@@ -0,0 +1,11 @@
+<div class="desc-block">
+  <img src="<?= get_theme_mod('img_desc') ?>">
+  <h3><?= get_theme_mod('title_desc_first') ?></h3>
+  <p><?= get_theme_mod('paragraph_desc_first') ?></p>
+
+  <h3><?= get_theme_mod('title_desc_second') ?></h3>
+  <p><?= get_theme_mod('paragraph_desc_second') ?></p>
+
+  <h3><?= get_theme_mod('title_desc_third') ?></h3>
+  <p><?= get_theme_mod('paragraph_desc_third') ?></p>
+</div>
