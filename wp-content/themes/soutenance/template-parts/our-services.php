8e0c925e2c03edce87fbd32f4d2aa21dacfd32b1
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-21 09:48:13 +0200

    feat(push-title)

--- wp-content/themes/soutenance/template-parts/our-services.php
+++ wp-content/themes/soutenance/template-parts/our-services.php
@@ -1,29 +1,29 @@
 <div class="our-services">
   <h2>Our Services</h2>
   <div class="services">
-    <a href="<?= get_theme_mod('link_first_services') ?>">
+    <a href="<?= get_permalink(get_theme_mod('link_first_services')) ?>">
       <?php if (!empty(get_theme_mod('image_first_services'))) : ?>
         <img src="<?= get_theme_mod('image_first_services') ?>" alt="">
       <?php endif; ?>
-      <h3><?= get_theme_mod('title_first_services') ?></h3>
+      <h3><?= get_theme_mod('text_first_services') ?></h3>
     </a>
-    <a href="<?= get_theme_mod('link_second_services') ?>">
+    <a href="<?= get_permalink(get_theme_mod('link_second_services')) ?>">
       <?php if (!empty(get_theme_mod('image_second_services'))) : ?>
         <img src="<?= get_theme_mod('image_second_services') ?>" alt="">
       <?php endif; ?>
-      <h3><?= get_theme_mod('title_second_services') ?></h3>
+      <h3><?= get_theme_mod('text_second_services') ?></h3>
     </a>
-    <a href="<?= get_theme_mod('link_third_services') ?>">
+    <a href="<?= get_permalink(get_theme_mod('link_third_services')) ?>">
       <?php if (!empty(get_theme_mod('image_third_services'))) : ?>
         <img src="<?= get_theme_mod('image_third_services') ?>" alt="">
       <?php endif; ?>
-      <h3><?= get_theme_mod('title_third_services') ?></h3>
+      <h3><?= get_theme_mod('text_third_services') ?></h3>
     </a>
-    <a href="<?= get_theme_mod('link_fourth_services') ?>">
+    <a href="<?= get_permalink(get_theme_mod('link_fourth_services')) ?>">
       <?php if (!empty(get_theme_mod('image_fourth_services'))) : ?>
         <img src="<?= get_theme_mod('image_fourth_services') ?>" alt="">
       <?php endif; ?>
-      <h3><?= get_theme_mod('title_fourth_services') ?></h3>
+      <h3><?= get_theme_mod('text_fourth_services') ?></h3>
     </a>
   </div>
 </div>
