f15ea4c98f2ec656913dc0ab151381eacf8b571a
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-21 02:50:45 +0200

    start-blog

--- wp-content/themes/soutenance/index.php
+++ wp-content/themes/soutenance/index.php
@@ -1,5 +1,12 @@
 <?php get_header('dark'); ?>
 <main id="site-content">
-  
+  <div id="post-list">
+    <?php
+    //Afficher les derniers articles si la page n'est pas la page d'accueil
+    if (!is_front_page()) {
+      include('template-parts/post-list.php');
+    }
+    ?>
+  </div>
 </main>
 <?php get_footer(); ?>
---
+++ wp-content/themes/soutenance/template-parts/post-list.php
@@ -0,0 +1,50 @@
+<div class="post-list-wrapper">
+	<?php
+	if(!isset($paged)){
+		$paged = (get_query_var('paged')) ? absint( get_query_var( 'paged' ) ) : 1;
+	}
+	
+
+	$args = [
+		'post_type' => 'post',
+		'posts_per_page' => get_option( 'posts_per_page' ),
+		'post_status' => 'publish',
+		'paged' => $paged
+	];
+
+	$the_query = new WP_Query($args);
+
+	if($the_query->have_posts()){
+		echo '<ul class="post-list">';
+		while($the_query->have_posts()){
+			$the_query->the_post();
+			$post = get_post();
+			echo '<li><a href="' . get_permalink($post->ID) . '">';
+			echo '<article>';
+			echo '<h2>' . $post->post_title . '</h1>';
+			echo '';
+			echo '</article></a></li>';
+		}
+		echo '</ul>';
+	}
+	echo '<div class="post-list-pagination">';
+	echo paginate_links([
+		'base' => get_permalink(get_option('page_for_posts')) . '%_%', 
+		'total' => $the_query->max_num_pages,
+		'current' => $paged
+	]);
+	echo '</div>';
+	?>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
