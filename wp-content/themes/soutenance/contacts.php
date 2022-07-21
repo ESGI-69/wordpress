d1c996dd8044a48ab1a4974c09a73add123693cf
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-21 03:48:43 +0200

    style

--- wp-content/themes/soutenance/contacts.php
+++ wp-content/themes/soutenance/contacts.php
@@ -4,23 +4,25 @@
 <h1 class="page-title"><?php the_title() ?></h1>
 <div class="page-content">
   <?php the_content() ?>
-  <div>
-    <span>Location</span>
-    <span><?= get_theme_mod("soutenance_adresse") ?></span>
-    <span><?= get_theme_mod("soutenance_code_postal") ?></span>
-    <span><?= get_theme_mod("soutenance_ville") ?></span>
-    <span><?= get_theme_mod("soutenance_pays") ?></span>
+  <div class="team">
+    <div class="location">
+      <span class="title">Location</span>
+      <span class="info"><?= get_theme_mod("soutenance_adresse") ?></span>
+      <span class="info"><?= get_theme_mod("soutenance_code_postal") ?> <?= get_theme_mod("soutenance_ville") ?> <?= get_theme_mod("soutenance_pays") ?></span>
+    </div>
+    <div class="team-member">
+      <span class="title"><?= get_theme_mod("name_role_fourth") ?></span>
+      <span class="info"><?= get_theme_mod("phone_role_fourth") ?></span>
+      <span class="info"><?= get_theme_mod("email_role_fourth") ?></span>
+    </div>
+    <div class="team-member">
+      <span class="title"><?= get_theme_mod("name_role_one") ?></span>
+      <span class="info"><?= get_theme_mod("phone_role_one") ?></span>
+      <span class="info"><?= get_theme_mod("email_role_one") ?></span>
+    </div>
   </div>
-  <div>
-    <span><?= get_theme_mod("name_role_fourth") ?></span>
-    <span><?= get_theme_mod("phone_role_fourth") ?></span>
-    <span><?= get_theme_mod("email_role_fourth") ?></span>
-  </div>
-  <div>
-    <span><?= get_theme_mod("name_role_one") ?></span>
-    <span><?= get_theme_mod("phone_role_one") ?></span>
-    <span><?= get_theme_mod("email_role_one") ?></span>
-  </div>
+</div>
+<div class="page-image">
   <?= get_the_post_thumbnail() ?>
 </div>
 <?php get_footer() ?>
--- wp-content/themes/soutenance/template-parts/our-team.php
+++ wp-content/themes/soutenance/template-parts/our-team.php
@@ -1,27 +1,29 @@
 <div class="our-team">
   <h2>Our Team</h2>
-  <div>
-    <img src="<?= get_theme_mod('photo_role_fourth') ?>" alt="">
-    <h3><?= get_theme_mod('name_role_fourth') ?></h3>
-    <p><?= get_theme_mod('phone_role_fourth') ?></p>
-    <p><?= get_theme_mod('email_role_fourth') ?></p>
-  </div>
-  <div>
-    <img src="<?= get_theme_mod('photo_role_three') ?>" alt="">
-    <h3><?= get_theme_mod('name_role_three') ?></h3>
-    <p><?= get_theme_mod('phone_role_three') ?></p>
-    <p><?= get_theme_mod('email_role_three') ?></p>
-  </div>
-  <div>
-    <img src="<?= get_theme_mod('photo_role_two') ?>" alt="">
-    <h3><?= get_theme_mod('name_role_two') ?></h3>
-    <p><?= get_theme_mod('phone_role_two') ?></p>
-    <p><?= get_theme_mod('email_role_two') ?></p>
-  </div>
-  <div>
-    <img src="<?= get_theme_mod('photo_role_one') ?>" alt="">
-    <h3><?= get_theme_mod('name_role_one') ?></h3>
-    <p><?= get_theme_mod('phone_role_one') ?></p>
-    <p><?= get_theme_mod('email_role_one') ?></p>
+  <div class="members">
+    <div class="member">
+      <img src="<?= get_theme_mod('photo_role_fourth') ?>" alt="">
+      <h3><?= get_theme_mod('name_role_fourth') ?></h3>
+      <p><?= get_theme_mod('phone_role_fourth') ?></p>
+      <p><?= get_theme_mod('email_role_fourth') ?></p>
+    </div>
+    <div class="member">
+      <img src="<?= get_theme_mod('photo_role_three') ?>" alt="">
+      <h3><?= get_theme_mod('name_role_three') ?></h3>
+      <p><?= get_theme_mod('phone_role_three') ?></p>
+      <p><?= get_theme_mod('email_role_three') ?></p>
+    </div>
+    <div class="member">
+      <img src="<?= get_theme_mod('photo_role_two') ?>" alt="">
+      <h3><?= get_theme_mod('name_role_two') ?></h3>
+      <p><?= get_theme_mod('phone_role_two') ?></p>
+      <p><?= get_theme_mod('email_role_two') ?></p>
+    </div>
+    <div class="member">
+      <img src="<?= get_theme_mod('photo_role_one') ?>" alt="">
+      <h3><?= get_theme_mod('name_role_one') ?></h3>
+      <p><?= get_theme_mod('phone_role_one') ?></p>
+      <p><?= get_theme_mod('email_role_one') ?></p>
+    </div>
   </div>
 </div>
