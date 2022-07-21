6adba2af088b85171e732496136a8af32cba66ba
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-21 10:16:12 +0200

    last-fixes

--- wp-content/themes/soutenance/contacts.php
+++ wp-content/themes/soutenance/contacts.php
@@ -21,6 +21,19 @@
       <span class="info"><?= get_theme_mod("email_role_one") ?></span>
     </div>
   </div>
+
+  <div class="form">
+    <h2>Write us here</h2>
+    <p>Don't be shy.</p>
+    <form action="" class="contact">
+      <input type="text" name="subject" id="subject" placeholder="subject">
+      <div class="coor">
+        <input type="email" name="email" id="email" placeholder="email">
+        <input type="tel" name="tel" id="tel" placeholder="Phone">
+      </div>
+      <textarea name="message" id="message" cols="30" rows="10" placeholder="Content"></textarea>
+    </form>
+  </div>
 </div>
 <div class="page-image">
   <?= get_the_post_thumbnail() ?>
